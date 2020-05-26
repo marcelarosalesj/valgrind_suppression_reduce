03a774c0e775d1f3d02a1b5629651d595189be89
Marcela Rosales <marcelarosalesj@gmail.com>
2020-05-26 19:09:15 +0000

    Updating memcheck_all_errors.supp file
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

--- src/parse.py
+++ src/parse.py
@@ -1,6 +1,6 @@
 import suppression as supp
 
-sf = supp.SuppressionFile("supp_files/may-25-2020.supp")
+sf = supp.SuppressionFile("supp_files/memcheck_all_errors_reduced.supp")
 parser = supp.Parser("supp_files/memcheck_all_errors.supp")
 
 parser.split_into(sf)
